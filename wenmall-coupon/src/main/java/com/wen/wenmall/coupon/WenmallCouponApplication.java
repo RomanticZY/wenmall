b996fd7554949bb882873d2f13ef331e8462db83
wenqs <1500276495@qq.com>
2022-11-27 17:52:46 +0800

    '微服务注册成功'

--- wenmall-coupon/src/main/java/com/wen/wenmall/coupon/WenmallCouponApplication.java
+++ wenmall-coupon/src/main/java/com/wen/wenmall/coupon/WenmallCouponApplication.java
@@ -2,7 +2,9 @@ package com.wen.wenmall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WenmallCouponApplication {
 
